06f1bee087604e04e1fe9992187b07b9be254c95
human6004 <letranhieunhanvn@gmail.com>
2025-03-02 16:11:44 +0700

    update buoi3 PART 2

--- src/buoi3/SDSinhVien.java
+++ src/buoi3/SDSinhVien.java
@@ -1,6 +1,59 @@
 package buoi3;
 
+import java.util.Scanner;
+
 public class SDSinhVien {
 
+	public static void main(String[] args) {
+		SinhVien s = new SinhVien();
+		s.nhap();
+		s.in();
+
+		// nhap danh sach sinh vien
+		SinhVien ds[];
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("\n Nhap so sinh vien: ");
+		int n = sc.nextInt();
+
+		ds = new SinhVien[n];
+		for (int i = 0; i < n; i++) {
+			System.out.println("\n Nhap sinh vien thu " + (i + 1) + ": ");
+			ds[i] = new SinhVien();
+			ds[i].nhap();
+		}
+
+		System.out.println("\n Danh sach sinh vien vua nhap:");
+		for (int i = 0; i < n; i++)
+			System.out.println(ds[i]);
+
+		System.out.println("\n Danh sach sinh vien bi canh bao:");
+		for (int i = 0; i < n; i++)
+			if (ds[i].dtb() < 1)
+				System.out.println(ds[i]);
+
+		System.out.println("\n Danh sach sinh vien co diem cao nhat:");
+		float max = Float.MIN_VALUE;
+		for (int i = 0; i < n; i++)
+			if (max < ds[i].dtb())
+				max = ds[i].dtb();
+
+		for (int i = 0; i < n; i++)
+			if (ds[i].dtb() == max)
+				System.out.println(ds[i]);
+
+		// sap sep sinh vien theo ten:
+		for (int i = 0; i < n - 1; i++)
+			for (int j = i + 1; j < n; j++)
+				if (ds[i].layTen().compareTo(ds[j].layTen()) < 0) {
+					SinhVien t = ds[i];
+					ds[i] = ds[j];
+					ds[j] = t;
+				}
+		System.out.println("\n Danh sach sinh vien da sap theo ten:");
+		for (int i = 0; i < n; i++)
+			System.out.println(ds[i]);
+
+	}
 
 }
--- src/buoi3/SinhVien.java
+++ src/buoi3/SinhVien.java
@@ -1,11 +1,13 @@
 package buoi3;
 
+import java.util.Scanner;
+
 import buoi2.Date;
 
 public class SinhVien {
 	private String msv, hten;
 	private Date nsinh;
-	private int n, max;
+	private int n, max;// n la so luong
 	private String mon[], diem[];
 
 	public SinhVien() {
@@ -21,7 +23,7 @@ public class SinhVien {
 	public SinhVien(String msv, String hten, Date nsinh, int n, int max, String[] mon, String[] diem) {
 		this.msv = new String(msv);
 		this.hten = new String(hten);
-		this.nsinh = new Date(nsinh) ;
+		this.nsinh = new Date(nsinh);
 		this.n = n;
 		this.max = max;
 		this.mon = new String[max];
@@ -41,4 +43,103 @@ public class SinhVien {
 			diem[i] = new String(s.diem[i]);
 		}
 	}
+
+	public void nhap() {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap mssv: ");
+		msv = sc.nextLine();
+		System.out.println("Nhap ho ten: ");
+		hten = sc.nextLine();
+		System.out.println("Nhap ngay sinh: ");
+		nsinh.nhap();
+		System.out.println("Nhap so luong mon hoc: ");
+		n = sc.nextInt();
+		if (n > max)
+			n = max;
+		sc.nextLine(); // loai bo phim enter
+		for (int i = 0; i < n; i++) {
+			System.out.println("\n Nhap mon thu " + (i + 1) + ":");
+			mon[i] = sc.nextLine();
+			System.out.println("\n Nhap diem mon thu " + (i + 1) + ":");
+			diem[i] = sc.nextLine();
+		}
+	}
+
+	public void in() {
+		System.out.println("[" + msv + ", " + hten + ", " + nsinh + ", + cac mon hoc: ");
+		// in cac mon hoc
+		for (int i = 0; i < n; i++) {
+			System.out.print(mon[i] + ":" + diem[i] + ", ");
+			System.out.print("]");
+		}
+	}
+
+	public String toString() {
+		String s = ("[" + msv + ", " + hten + ", " + nsinh + ", + cac mon hoc: ");
+		// in diem mon hoc
+		for (int i = 0; i < n; i++)
+			s += (mon[i] + ":" + diem[i] + ",");
+		s += ("]");
+
+		return s;
+	}
+
+	public float dtb() {
+		float d = 0.0f;
+		for (int i = 0; i < n; i++) {
+			if (diem[i].equals("A"))
+				d += 4;
+			else if (diem[i].equals("B+"))
+				d += 3.5;
+			else if (diem[i].equals("B"))
+				d += 3;
+			else if (diem[i].equals("C+"))
+				d += 2.5;
+			else if (diem[i].equals("C"))
+				d += 2;
+			else if (diem[i].equals("D"))
+				d += 1.5;
+			else if (diem[i].equals("B+"))
+				d += 1;
+		}
+		return (d / n);
+	}
+
+	public void them(String m, String d) {
+		if (n < max) { // them mon vao cuoi dsach
+			mon[n] = new String(m);
+			diem[n] = new String(d);
+			n++;
+		}
+	}
+
+	public void xoa(String m) {
+		int i = 0;
+		for (i = 0; i < n; i++) { // tim kiem mon hoc m
+			if (mon[i].equals(m))
+				break;
+		}
+
+		if (i < n) { // tim thay mon m
+			for (int j = i; j < n - 1; j++) {
+				mon[j] = new String(mon[j + 1]);
+				diem[j] = new String(diem[j + 1]);
+			}
+			n--;
+		}
+	}
+
+	public String layTen() {
+		hten = hten.trim();
+		return hten.substring(hten.lastIndexOf(" ") + 1);
+	}
+
+	public String layEmail() {
+		return "";
+	}
+
+	public String layTKhoan() {
+		return "";
+	}
+
 }
