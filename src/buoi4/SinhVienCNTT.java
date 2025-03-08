224b49c28c06f5a8d6b21aee7a586b52b1c54c2d
human6004 <letranhieunhanvn@gmail.com>
2025-03-08 09:01:01 +0700

    add buoi4

---
+++ src/buoi4/SinhVienCNTT.java
@@ -0,0 +1,23 @@
+package buoi4;
+
+import buoi3.SinhVien;
+
+public class SinhVienCNTT extends SinhVien {
+	public String taikhoan;
+	public String matkhau;
+	public String email;
+	
+	
+	public SinhVienCNTT() {
+		super();
+		taikhoan = new String();
+		matkhau = new String();
+		email = new String();
+	}
+	
+	
+	
+	
+	
+
+}
